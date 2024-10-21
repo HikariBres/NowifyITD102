8eafc8fc12cb6234c25c5abaf6c96e2e9f0ebc1b
HikariBres <hikaribres@gmail.com>
2024-10-21 23:32:29 +1000

    Update App.vue
    trackAlbumName

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,8 @@ export default {
         playing: false,
         trackArtists: [],
         trackTitle: '',
-        trackAlbum: []
+        trackAlbum: [],
+        trackAlbumName: ''
       },
       storedId: ''
     }
